d22e8e18d855448c67d075fc2feaa46921f65ba5
Georgiy95 <urijpetrovskij@gmail.com>
2023-06-21 11:48:14 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,16 +8,16 @@ const galleryCardsSet = createGallery(galleryItems);
 function createGallery(galleryItems) {
   return galleryItems
     .map(({ original, preview, description }) => {
-      return `<div class="gallery__item" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+      return `<div class="gallery__item" style= "box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
     7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
-  <a class="gallery__link" href="${original}" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+  <a class="gallery__link" href="${original}" style= "box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
     7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
-      style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+      style= "box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
     7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)"
     />
   </a>
