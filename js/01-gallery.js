ce54f5cfeabcd6b7950b606b8f3c57743b371877
Georgiy95 <urijpetrovskij@gmail.com>
2023-06-21 11:53:02 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -35,7 +35,7 @@ function selectGalleryEl(event) {
     return;
   }
   const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" width="800" height="600" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    `<img src="${event.target.dataset.source}" width="800" height="600" style= "box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
     7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">`,
 
     {
